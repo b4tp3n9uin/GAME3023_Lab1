05d3b74c9c528ef38e3de52ce84984f273082d08
Matthew Makepeace <Matthewmakepeace32@gmail.com>
2021-10-04 15:59:15 -0400

    Lab 3 - Added Starting screen and UI Buttons

---
+++ Assets/Scripts/UIButtonScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonScript : MonoBehaviour
+{
+    public void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OnExitButtonPressed()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit!");
+    }
+    public void OnMenuButtonPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
